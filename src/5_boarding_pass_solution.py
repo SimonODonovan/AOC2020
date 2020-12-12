dc1e77549edab8bd5f4c18df61c8c9c388028eee
Stga <contact.sodonovan@gmail.com>
2020-12-12 14:54:33 +0000

    Day5 Part 2

--- src/5_boarding_pass_solution.py
+++ src/5_boarding_pass_solution.py
@@ -21,10 +21,25 @@ def generate_seat_id(row, col):
 
 seating_instructions_list = utils.read_input('5_boarding_pass_input.txt', str)
 max_seat_id = -1
+occupied_seats = list()
+
+# Part 1
 for seating_instruction in seating_instructions_list:
     row = get_seating(seating_instruction[0:-3], 127)
     col = get_seating(seating_instruction[-3:], 7)
     seat_id = generate_seat_id(row, col)
     if seat_id > max_seat_id:
         max_seat_id = seat_id
+        print(seating_instruction)
+    occupied_seats.append(seat_id)
 print(max_seat_id)
+
+# Part 2
+known_seats = set(range(0, max_seat_id))
+empty_seats = known_seats.difference(set(occupied_seats))
+sorted_empty_seats = sorted(list(empty_seats))
+for id,seat in enumerate(sorted_empty_seats[0:-1]):
+    is_sequential = sorted_empty_seats[id+1] == seat+1 # check for non-sequential seat id.
+    if not is_sequential:
+        designated_seat = sorted_empty_seats[id+1]
+print(designated_seat)
