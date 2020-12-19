e3f1a53a982a80eaef596eeab5cd7454457ec9e6
Stga <contact.sodonovan@gmail.com>
2020-12-19 22:35:41 +0000

    Day 9 Part 2

--- src/9_xmas_encryption_solution.py
+++ src/9_xmas_encryption_solution.py
@@ -15,13 +15,34 @@ def check_xmas_weakness(current, preamble):
     return True
 
 # Part 1
+invalid_number = -1
 for idx, current in enumerate(input[25:]):
     is_weak = check_xmas_weakness(current, input[range_min:range_max])
     if is_weak:
-        print(current)
+        invalid_number = current
         break
     else:
         range_min += 1
         range_max += 1
+print(invalid_number)
 
 # Part 2
+exploit_range = list()
+input.remove(invalid_number)
+for idx,val in enumerate(input):
+    if exploit_range:
+        break
+    acc = 0
+    for idx2,val2 in enumerate(input[idx:]):
+        acc += val2
+        if acc == invalid_number:
+            range_end = idx+idx2
+            exploit_range = input[idx:range_end]
+            break
+        elif acc > invalid_number:
+            break
+
+min = min(exploit_range)
+max = max(exploit_range)
+exploit = min + max
+print(exploit)
